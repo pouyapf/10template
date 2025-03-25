5b96060d8f35c8576e5495f8debd5ded722073a5
pouya <pouya.ff7@gmail.com>
2025-03-25 18:20:55 +0430

    3nddd commit

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import LandingPage from '@/Components/Template2/LandingPage1/LandingPage1'
 import Link from 'next/link'
 function Home() {
   return (
-    <div className=' flex justify-center items-center gap-2 flex-col min-h-screen'>
-<Link href={'/template1'} className=' text-black text-center border border-btn3 rounded min-w-20 p-2' >Theme1</Link>
-<Link href={'/template2'} className='text-black text-center border border-btn3 rounded min-w-20 p-2' >Theme2</Link>
-<Link href={'/template3'} className='text-black text-center border border-btn3 rounded min-w-20 p-2' >Theme3</Link>
+    <div className=' bg-black flex justify-center items-center gap-2 flex-col min-h-screen'>
+<Link href={'/template1'} className='  text-white text-center border border-btn3 rounded min-w-20 p-2' >Theme1</Link>
+<Link href={'/template2'} className=' text-white text-center border border-btn3 rounded min-w-20 p-2' >Theme2</Link>
+<Link href={'/template3'} className=' text-white text-center border border-btn3 rounded min-w-20 p-2' >Theme3</Link>
 
 
     </div>
